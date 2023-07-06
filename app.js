7884fb9025faf379b6e610f8706a9ce9b8842b3d
julesltxs <133399041+julesltxs@users.noreply.github.com>
2023-07-06 00:39:31 -0500

    ss

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var navbarContainer = document.getElementById("navbar-container");
-
-    if (navbarContainer) {
-        fetch('navbar.html')
-            .then(response => response.text())
-            .then(data => {
-                navbarContainer.innerHTML = data;
-                makeSticky(); // Call makeSticky again to apply sticky effect to dynamically loaded navbar
-            });
-    }
-});
-
 window.onscroll = function() {makeSticky()};
 
 var navbar = document.getElementById("navbar");
-var sticky = navbar ? navbar.offsetTop : 0;
+var sticky = navbar.offsetTop;
 
 function makeSticky() {
-    if (!navbar) {
-        navbar = document.getElementById("navbar");
-        sticky = navbar ? navbar.offsetTop : 0;
-    }
-    
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
 }
+
+// Load the navbar
+document.addEventListener("DOMContentLoaded", function () {
+    var navbarContainer = document.getElementById("navbar-container");
+    
+    if (navbarContainer) {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => navbarContainer.innerHTML = data);
+    }
+});
 
 
